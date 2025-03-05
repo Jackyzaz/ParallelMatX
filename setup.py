4e16c546252be30b95e232b71d7b68336090a016
Soravit Sukkarn <soravit.sukkarn@gmail.com>
2025-03-05 18:41:26 +0700

    update: 1.0.1 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="parallematx",
-    version="1.0",
+    version="1.0.1",
     description="Parallel Matrix Multiplication with ProcessPoolExecutor",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
